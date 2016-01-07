14a72692b04bd8dbcfd552d1a475249b9718400e
andela-gjames <george.james@andela.com>
2016-01-07 11:47:18 +0100

    (Feature) Added EmojiKeyword model
    The emojiKeyword model represents the keywords that are associated to the emoji model

---
+++ src/models/EmojiKeyword.php
@@ -0,0 +1,15 @@
+<?php
+namespace BB8\Emoji\Models;
+use \Illuminate\Database\Eloquent\Model as BaseModel;
+
+class EmojiKeyword extends BaseModel
+{
+    public $timestamps  = false;
+    protected $table    = 'keywords';
+    protected $fillable = ['name', 'emoji_id'];
+
+    public function emojis()
+    {
+        return $this->belongsTo("BB8\Emoji\Models\Emoji");
+    }
+}
