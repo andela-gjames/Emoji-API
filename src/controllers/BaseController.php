758ff5d49e2d93c3aa565e10cdf7cea121ab4002
andela-gjames <george.james@andela.com>
2016-01-07 21:43:03 +0100

    (Feature) Added getMessage method and authenticateRouteResquest method
    The getMessage route returns the correct message based on the status of a request
    and also sets the request status type.
    
    The authenticateRouteResquest method is used to validate the token return by user request

--- src/controllers/BaseController.php
+++ src/controllers/BaseController.php
@@ -4,11 +4,53 @@ use BB8\Emoji\Database\Connection;
 
 class BaseController
 {
-    public function __construct()
+    const AUTHENTICATIONERROR   = 'Username or password incorrect';
+    const BADREQUESTERROR       = 'Bad request';
+
+    protected $auth;
+    public function __construct($container)
     {
+        $this->auth = $container->get('auth');
         $dotenv = new \Dotenv\Dotenv(__DIR__.'/../../');
         $dotenv->load();
 
         $connection = new Connection();
+    }
+
+    protected function authenticateRouteResquest($token, $jit, &$response, $successMessage)
+    {
+        $result = $this->auth->isTokenValid($token, $jit);
+        switch($result) {
+            case 'Expired':
+                $response = $response->withStatus(401);
+                $message    =   [
+                        'status' => 'Error',
+                        'message' => 'Unathorized Access: Token has expired'
+                    ];
+                return $message;
+            case 'Invalid Token Version':
+                $response = $response->withStatus(401);
+                $message    =   [
+                        'status' => 'Error',
+                        'message' => 'Unathorized Access: Usage of wrong token version'
+                    ];
+                return $message;
+            default:
+                $message    =   [
+                        'status' => 'success',
+                        'message' => $successMessage
+                    ];
+                return $message;
+        }
+    }
+
+    protected function getMessage($message, &$response, $errorStatus = null)
+    {
+        $type = ($errorStatus == null) ? 'success' : 'error';
+        $response = ($errorStatus == null) ? $response->withStatus(200) : $response->withStatus($errorStatus);
+        return [
+            'status'    => $type,
+            'message'   => $message
+        ];
     }
 }
