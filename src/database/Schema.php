d0e39bd9256075a19a430fd0b8f51d47ded42925
andela-gjames <george.james@andela.com>
2016-01-07 11:42:16 +0100

    (Feature) Added Schema
    To help build the database, a schema file is called and helps to create the neccessary tables for the database

---
+++ src/database/Schema.php
@@ -0,0 +1,41 @@
+<?php
+namespace BB8\Emoji\Database;
+
+use Illuminate\Database\Capsule\Manager as Capsule;
+
+class Schema
+{
+    public static function createSchema()
+    {
+        if (!Capsule::schema()->hasTable('users')) {
+            Capsule::schema()->create('users', function($table){
+                $table->increments('id');
+                $table->string('username')->unique();
+                $table->string('password');
+                $table->integer('jit')->nullable();
+                $table->timestamps();
+            });
+        }
+
+        if (!Capsule::schema()->hasTable('emojis')) {
+            Capsule::schema()->create('emojis', function($table) {
+                $table->increments('id');
+                $table->string('name');
+                $table->string('char');
+                $table->string('category');
+                $table->integer('user_id');
+                $table->foreign('user_id')->references('id')->on('users');
+                $table->timestamps();
+            });
+        }
+
+        if (!Capsule::schema()->hasTable('keywords')) {
+            Capsule::schema()->create('keywords', function($table) {
+                $table->increments('id')->onDelete('cascade');
+                $table->string('name')->onDelete('cascade');
+                $table->integer('emoji_id')->onDelete('cascade');
+                $table->foreign('emoji_id')->references('id')->on('emoji')->onDelete('cascade');
+            });
+        }
+    }
+}
