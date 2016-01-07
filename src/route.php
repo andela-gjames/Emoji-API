8a68cc9d8180666c51cab46eec113f3a3a5bbf6a
andela-gjames <george.james@andela.com>
2016-01-07 10:41:07 +0100

    (Feature) Added route file
    Added route file to be used to route to different parts of the app

---
+++ src/route.php
@@ -0,0 +1,19 @@
+<?php
+require 'vendor/autoload.php';
+use \Slim\App;
+use BB8\Emoji\Database\Connection;
+use BB8\Emoji\Models\User;
+use BB8\Emoji\Database\Schema;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+
+$connection = new Connection();
+Schema::createSchema();
+$configuration = [
+    'settings' => [
+        'displayErrorDetails' => true,
+    ],
+];
+$app = new App($configuration);
+
+$app->run();
