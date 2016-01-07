6587146dae02eadb5227ebf907790e753d6e06fc
andela-gjames <george.james@andela.com>
2016-01-07 10:51:29 +0100

    (Feature) Added Login capability
    Login capability has been added to enable users of the API login into the site
    which returns a json token for subsequent request

--- src/controllers/UserController.php
+++ src/controllers/UserController.php
@@ -24,4 +24,58 @@ class UserController extends BaseController
         $response_body->write(json_encode(['message'=>'User Created']));
         return $response;
     }
+
+    public function login(ServerRequestInterface $request, ResponseInterface $response)
+    {
+        $response           =   $response->withAddedHeader('Content-type', 'application/json');
+        $data               =   $request->getParsedBody();
+        $response_body      =   $response->getBody();
+
+        if (array_key_exists('username', $data) && array_key_exists('password', $data)) {
+            $user   =   User::auth($data['username'], $data['password']);
+
+            if (!!$user) {
+                $response   =   $response->withStatus(200);
+
+                $iat        =   time();
+                $jit        =   rand(1000, 999999999);
+                $exp        =   $iat + 1000000000;
+
+                //Ensures user is not logged out from other device
+                $user->jit  =   $user->jit == null ? $jit : $user->jit;
+                $user->save();
+
+                $data       =   [
+                    'jit'   =>  $jit,
+                    'iat'   =>  $iat,
+                    'exp'   =>  $exp,
+                    'data'  =>  [
+                                    'uid'       =>  $user->id,
+                                    'username'  =>  $user->username
+                                ]
+                ];
+
+                $token      =   JWT::encode($data, getenv('SECRET_KEY'));
+                $message = [
+                  'token'   =>  $token
+                ];
+            } else {
+                $response   =   $response->withStatus(401);
+                $message    =   [
+                    'message'   =>  'Validation failed',
+                    'errors'    =>  [
+                        [
+                            'message'   =>  'Username or password incorrect'
+                        ]
+                    ]
+                ];
+            }
+        } else {
+            $response   =   $response->withStatus(400);
+            $message    =   ['message'   =>  'Bad Request'];
+        }
+
+        $response_body->write(json_encode($message));
+        return $response;
+    }
 }
