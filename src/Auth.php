fabf7a10b965c92a14abf47ba25f67706e7c2927
andela-gjames <george.james@andela.com>
2016-01-07 21:45:01 +0100

    (Feature) Created auth.php
    This class provides authentication service for token, it verifies that token is valid and not expired
    and also that the corret token version has been returned

---
+++ src/Auth.php
@@ -0,0 +1,43 @@
+<?php
+namespace BB8\Emoji;
+
+use Firebase\JWT\JWT;
+use Carbon\Carbon;
+class Auth
+{
+    public function isTokenValid($token, $jit)
+    {
+        $result = null;
+        if($this->isTokenExpired($token) == true) {
+            $result = "Expired";
+        } elseif($this->isTokenJITValid($token, $jit) == false) {
+            $result = "Invalid Token Version";
+        }
+        return $result;
+    }
+
+    public function isTokenExpired($token)
+    {
+        $token          =   $this->decodeToken($token);
+
+        $exp            =   date('Y-m-d H:m:s', $token->exp);
+        $expireDate     =   new Carbon($exp);
+
+        return Carbon::now()->gte($expireDate);
+    }
+
+    public function isTokenJITValid($token, $jit)
+    {
+        $token  =   $this->decodeToken($token);
+
+        return intval($token->jit) === intval($jit);
+    }
+
+    private function decodeToken($token)
+    {
+        $token             =   str_replace("Bearer ", "", $token);
+        $token          =   JWT::decode($token, getenv('SECRET_KEY'), array('HS256'));
+
+        return $token;
+    }
+}
