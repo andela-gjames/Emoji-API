29663e2398f0d049c88c3f07c6d00a637ad4a14e
andela-gjames <george.james@andela.com>
2016-01-07 11:20:36 +0100

    (Feature) Added BaseModel
    BaseModel added to provide common config for all models  that extends it

---
+++ src/models/BaseModel.php
@@ -0,0 +1,13 @@
+<?php
+namespace BB8\Emoji\Models;
+use BB8\Emoji\Database\Connection;
+
+class BaseModel extends \Illuminate\Database\Eloquent\Model
+{
+    public function __construct(array $attribute = [])
+    {
+        parent::__construct($attribute);
+        $dotenv = new \Dotenv\Dotenv(__DIR__.'/../../');
+        $dotenv->load();
+    }
+}
